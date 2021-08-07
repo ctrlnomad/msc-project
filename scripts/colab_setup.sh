738468813e31989951fdabb0c71216546c58ad4b
Sultan Kenjeyev <ksula0155@gmail.com>
2021-08-07 17:13:42 +0100

    aggrr

--- scripts/colab_setup.sh
+++ scripts/colab_setup.sh
@@ -2,5 +2,5 @@ git config --global user.email "ksula0155@gmail.com"
 git config --global user.name "Sultan Kenjeyev"
 git checkout causal_mnist
 pip3 install argparse_dataclass
-python -m utils.mnist
+python3 -m utils.mnist
 
